b280e4ae7e9c1b3f794dbf9a1dd5853fe892c5c2
Asgedom <asgedomhail@gmail.com>
2023-08-09 17:41:27 -0700

    update build error

--- src/component/PlatformSelector.tsx
+++ src/component/PlatformSelector.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { Button,  Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 import { Platform } from '../hooks/useGames'
 import usePlatforms from '../hooks/usePlatforms'
